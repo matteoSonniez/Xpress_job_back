41e3360e889cea21067599d7cc8bb3a26503cc5f
sonniez matteo <sonniezmatteo@device-267.home>
2025-03-18 14:08:24 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose');
+var cors = require('cors')
+//const apiRouter = require('./routes');
+require('dotenv').config();
+
+//mongoDb connect
+mongoose.set("strictQuery", false);
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority` 
+).then(() => {
+  console.log("successfully connect to database")
+}).catch(err=>console.log(err))
+
+//Middlewares & routes
+app.use(cors());
+//app.use("/api/", apiRouter)
+
+// start app
+app.listen(process.env.PORT, function () {
+  console.log("Server launch");
+}); 
